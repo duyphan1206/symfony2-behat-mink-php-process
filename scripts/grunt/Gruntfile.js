f6e96ccfde16fbc8bd1653bd313433cbb7e8b63b
Nguyen Trinh Anh Hau <hau.nguyentrinh@asnet.com.vn>
2013-10-05 14:14:23 +0700

    add grunt build for ant build

--- scripts/grunt/Gruntfile.js
+++ scripts/grunt/Gruntfile.js
@@ -9,16 +9,16 @@ module.exports = function (grunt) {
 
     s3: {
          options: {
-		  key: 'AKIAJLSP2MXGY2AOKDDQ',
-		  secret: '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',
-		  bucket: 'php-dev-demo',
-		  access: 'public-read',
-		  headers: {
-			// Two Year cache policy (1000 * 60 * 60 * 24 * 730)
-			"Cache-Control": "max-age=630720000, public",
-			"Expires": new Date(Date.now() + 63072000000).toUTCString()
-		  }
-		},
+        key: 'AKIAJLSP2MXGY2AOKDDQ',
+        secret: '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',
+        bucket: 'php-dev-demo',
+        access: 'public-read',
+        headers: {
+        // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
+        "Cache-Control": "max-age=630720000, public",
+        "Expires": new Date(Date.now() + 63072000000).toUTCString()
+      }
+    },
       test: {
         options: {}
       },
@@ -28,18 +28,29 @@ module.exports = function (grunt) {
         }
       },
       test_S3Task: {
-        upload: [{
-			src: 'src/**/*.*',
-			dest: '/src/',
-			rel: 'src/'
-		  }]
+          upload: [{
+          src: '../../web/Symfony/**/*.*',
+          dest: '/Symfony/',
+          rel: '../../web/Symfony/'
+        }]
+      }
+    },
+    replace: {
+      example: {
+        src: ['../../web/Symfony/src/Dev/TaskBundle/Resources/views/Task/*.*'],             // source files array (supports minimatch)
+        dest: '../../web/Symfony/src/Dev/TaskBundle/Resources/views/Task/',             // destination directory or file
+        replacements: [{
+          from: 'Task list',                   // string replacement
+          to: 'anhhau'
+        }]
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-s3');
-  
+  grunt.loadNpmTasks('grunt-text-replace');
+
     grunt.registerTask('default', [
-        's3'
+        'replace'
     ]);
 };
