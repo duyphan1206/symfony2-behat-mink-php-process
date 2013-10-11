db37ff5d8d032255de7f55e04b9554b7dc1ea9c5
Nguyen Trinh Anh Hau <hau.nguyentrinh@asnet.com.vn>
2013-10-11 08:57:59 +0700

    fix script: build for composer and grunt

--- scripts/grunt/Gruntfile.js
+++ scripts/grunt/Gruntfile.js
@@ -29,61 +29,67 @@ module.exports = function (grunt) {
       },
       test_S3Task: {
           upload: [{
-            src: '../../web/Symfony/app/*.*',
-            dest: '/Symfony/app/',
-            rel: '../../web/Symfony/app/'
-          },
-          {
-            src: '../../web/Symfony/app/config/**/*.*',
-            dest: '/Symfony/app/config/',
-            rel: '../../web/Symfony/app/config/'
-          },
-          {
-            src: '../../web/Symfony/app/Resources/**/*.*',
-            dest: '/Symfony/app/Resources/',
-            rel: '../../web/Symfony/app/Resources/'
-          },
-          {
-            src: '../../web/Symfony/bin/**/*.*',
-            dest: '/Symfony/bin/',
-            rel: '../../web/Symfony/bin/'
-          },
-          {
-            src: '../../web/Symfony/src/**/*.*',
-            dest: '/Symfony/src/',
-            rel: '../../web/Symfony/src/'
-          },
-          {
-            src: '../../web/Symfony/web/**/*.*',
-            dest: '/Symfony/web/',
-            rel: '../../web/Symfony/web/'
-          },
-          {
-            src: '../../web/Symfony/*.*',
-            dest: '/Symfony/',
-            rel: '../../web/Symfony/'
-          }
-
-          ]
+            src: '../../web/Symfony/web/bundles/devtask/css/main.css',
+            dest: '/css/main.min.css',
+          }]
       }
     },
-    replace: {
-      example: {
-        src: ['../../web/Symfony/src/Dev/TaskBundle/Resources/views/layout.html.twig'],             //  source files array (supports minimatch)
-        dest: '../../web/Symfony/src/Dev/TaskBundle/Resources/views/layout.html.twig',             // destination directory or file
-        replacements: [{
-          from: "{{ asset('bundles/devtask/css/task.css') }}",                   // string replacement
-          to: 'https://php-dev-demo.s3.amazonaws.com/Symfony/web/bundles/devtask/css/task.css'
-        }]
+
+
+
+    concat: {
+
+      styles: {
+        src: '../../web/Symfony/web/bundles/devtask/css/**/*.css',
+        dest: '../../web/Symfony/web/bundles/devtask/css/main.css'
       }
-    }
+    },
+
+    // Minify _everything_. Same logic as the concat step, just smaller files.
+    uglify: {
+      styles: {
+        src: '../../web/Symfony/web/bundles/devtask/css/main.css',
+        dest: '../../web/Symfony/web/bundles/devtask/css/main.min.css'
+      }
+    },
+
+     // renames JS/CSS to prepend a hash of their contents for easier
+    // versioning
+    rev: {
+      css: '../../web/Symfony/web/bundles/devtask/css/**/*.css',
+    },
+
+    useminPrepare: {
+      html: ['../../**/*.html.twig']
+    },
+    // update references in HTML/CSS to revved files
+    usemin: {
+      html: ['../../web/Symfony/src/Dev/TaskBundle/**/*.html.twig']
+    },
+
+    // replace: {
+    //   example: {
+    //     src: ['../../web/Symfony/src/Dev/TaskBundle/Resources/views/layout.html.twig'],             //  source files array (supports minimatch)
+    //     dest: '../../web/Symfony/src/Dev/TaskBundle/Resources/views/layout.html.twig',             // destination directory or file
+    //     replacements: [{
+    //       from: "{{ asset('bundles/devtask/css/task.css') }}",                   // string replacement
+    //       to: 'https://php-dev-demo.s3.amazonaws.com/Symfony/web/bundles/devtask/css/task.css'
+    //     }]
+    //   }
+    // }
   });
 
   grunt.loadNpmTasks('grunt-s3');
-  grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-rev');
 
     grunt.registerTask('default', [
-        'replace',
+
+        'concat',
+        'rev',
+        'usemin',
         's3'
     ]);
 };
