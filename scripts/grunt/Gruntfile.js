d4ecb0d8cc1955869a7f1c5116eaf8145026e0a9
Cong Dang <congdang@asnet.com.vn>
2013-10-11 16:56:13 +0700

    Fixed grunt scripts

--- scripts/grunt/Gruntfile.js
+++ scripts/grunt/Gruntfile.js
@@ -1,4 +1,14 @@
 // Generated on 2013-06-12 using generator-webapp 0.2.3
+
+/**
+  * Step 1: Copy the code that should be deploy to production server
+  * Step 2: Minify the assets (css/js)
+  * Step 3: Concat the version to assets files
+  * Step 4: Upload the assets to S3
+  * Step 5: Replace assets in html/template files.
+  * Step 6: Deploy the dist folder to production server
+  * Step 7: Clear dist folder
+  */
 'use strict';
 
 var path = require('path');
@@ -6,90 +16,162 @@ var path = require('path');
 module.exports = function (grunt) {
 
   grunt.initConfig({
-
-    s3: {
-         options: {
-        key: 'AKIAJLSP2MXGY2AOKDDQ',
-        secret: '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',
-        bucket: 'php-dev-demo',
-        access: 'public-read',
-        headers: {
-        // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
-        "Cache-Control": "max-age=630720000, public",
-        "Expires": new Date(Date.now() + 63072000000).toUTCString()
-      }
+     symfony: {
+        app: '../../Symfony',
+        dist: '../../dist'
     },
-      test: {
-        options: {}
-      },
-      test_options: {
-        options: {
-          key: 'custom'
+    s3: {
+       options: {
+          key: 'AKIAJLSP2MXGY2AOKDDQ',
+          secret: '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',
+          bucket: 'php-dev-demo',
+          access: 'public-read',
+          headers: {
+          // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
+          "Cache-Control": "max-age=630720000, public",
+          "Expires": new Date(Date.now() + 63072000000).toUTCString()
         }
       },
-      test_S3Task: {
+        
+      assets_deploy: {
           upload: [{
-            src: '../../web/Symfony/web/bundles/devtask/css/main.css',
-            dest: '/css/main.min.css',
+            src: 'dist/web/bundles/devtask/css/**',
+            // a prefix folder on S3
+            dest:  'assets/',
+            // the rel option
+            rel: 'dist/web/'
           }]
       }
     },
 
-
+    // Put files not handled in other tasks here
+    copy: {
+       
+        dist: {
+            files: [{
+                expand: true,
+                dot: true,
+                cwd: '<%= symfony.app %>',
+                dest: '<%= symfony.dist %>',
+                    src: [
+                        '*.*',
+                        'app/*',
+                        'app/config/**/*',
+                        'app/Resources/**/*',
+                        'app/Resources/**/*',
+                        'src/**/*',
+                        'web/**/*'
+                    ]
+            }]
+        }
+    },
 
     concat: {
-
       styles: {
-        src: '../../web/Symfony/web/bundles/devtask/css/**/*.css',
-        dest: '../../web/Symfony/web/bundles/devtask/css/main.css'
+        src: '../../dist/web/bundles/devtask/css/**/*.css',
+        dest: '../../dist/web/bundles/devtask/css/main.min.css'
       }
     },
 
     // Minify _everything_. Same logic as the concat step, just smaller files.
     uglify: {
       styles: {
-        src: '../../web/Symfony/web/bundles/devtask/css/main.css',
-        dest: '../../web/Symfony/web/bundles/devtask/css/main.min.css'
+        src: '../../dist/web/bundles/devtask/css/main.css',
+        dest: '../../dist/web/bundles/devtask/css/main.min.css'
       }
     },
 
-     // renames JS/CSS to prepend a hash of their contents for easier
-    // versioning
-    rev: {
-      css: '../../web/Symfony/web/bundles/devtask/css/**/*.css',
-    },
-
-    useminPrepare: {
-      html: ['../../**/*.html.twig']
+     useminPrepare: {
+      html: ['../../dist/src/Dev/TaskBundle/Resources/views/*.html.twig']
     },
     // update references in HTML/CSS to revved files
     usemin: {
-      html: ['../../web/Symfony/src/Dev/TaskBundle/**/*.html.twig']
+      html: ['../../dist/src/Dev/TaskBundle/Resources/views/layout.html.twig']
     },
 
-    // replace: {
-    //   example: {
-    //     src: ['../../web/Symfony/src/Dev/TaskBundle/Resources/views/layout.html.twig'],             //  source files array (supports minimatch)
-    //     dest: '../../web/Symfony/src/Dev/TaskBundle/Resources/views/layout.html.twig',             // destination directory or file
-    //     replacements: [{
-    //       from: "{{ asset('bundles/devtask/css/task.css') }}",                   // string replacement
-    //       to: 'https://php-dev-demo.s3.amazonaws.com/Symfony/web/bundles/devtask/css/task.css'
-    //     }]
-    //   }
-    // }
+    assets: {
+      // global task options
+      options: {
+        // don't output debug information
+        debug: false,
+        // trancate the hash length to 8 chars.
+        truncateHash: 8,
+        // define the location of the manifest file.
+        manifest: 'temp/manifest.json',
+        // define the location of the cdn.
+        cdnurl: 'https://php-dev-demo.s3.amazonaws.com/assets/',
+        // Set maximum number of file copying concurent operations.
+        maxOperations: 100,
+        // Show a fancy progress indicator
+        progress: true,
+        // Set path to substract so the relative path for the assets can be calculated.
+        rel: 'bundles/devtask/css/'
+      },
+
+      all: {
+        // local task options
+        options: {
+          // change the rel path to test/case
+          rel: '../../dist/web/'
+        },
+        src: [
+          // all files under folder assets
+          '../../dist/web/bundles/devtask/css/*.min.css',
+        ],
+        dest: 'temp/assets'
+      }
+    },
+
+
+    assetsReplace: {
+      // global task options
+      options: {
+        // don't output debug information
+        debug: false,
+        // define the location of the manifest file.
+        manifest: 'temp/manifest.json',
+      },
+      twig: {
+        options: {
+          // a regex for lax matching {{asset "%"}} allowing for spaces in between
+          // and single quotes.
+          key: "__ASSET(%)"
+        },
+        // all the files from the assets/handlebars folder
+        src: '../../dist/src/Dev/TaskBundle/Resources/views/layout.html.twig',
+        // output to temp/handlebars folder
+        dest: '../../dist/src/Dev/TaskBundle/Resources/views/layout.html.twig'
+      }
+    },
+
+    clean: {
+        dist: {
+            files: [{
+                dot: true,
+                src: [
+                    'temp',
+                    'dist'
+                    // '<%= symfony.dist %>/**/*'
+                ]
+            }]
+        }
+    }
   });
 
-  grunt.loadNpmTasks('grunt-s3');
+  
   grunt.loadNpmTasks('grunt-contrib-concat');
-  // grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-usemin');
-  grunt.loadNpmTasks('grunt-rev');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('assetflow');
+  grunt.loadNpmTasks('grunt-s3');
 
     grunt.registerTask('default', [
-
-        'concat',
-        'rev',
-        'usemin',
-        's3'
+        'copy',
+        'concat','usemin',
+        'assets',
+        'assetsReplace',
+        's3', 'clean'
     ]);
 };
